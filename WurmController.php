ed7c334fe73ce7eeaca38a183f2f45c5fea6c3bc
Marlon B v/d Linde <marlon@kbye.co.za>
2017-09-20 19:50:17 +0200

    HTTP status code constants.

--- WurmController.php
+++ WurmController.php
@@ -12,6 +12,11 @@ class WurmController {
     const SQLITE_DIR = "sql/";                      // where the wurm db sqlite files are located
     const SQLITE_DB_PLAYERS = "wurmplayers.db";     // The players database
     const BLOCK_LOGGING = true;                     // whether logging is needed. This is not good really, leave on true
+    const HTTP_OK = 200;
+    const HTTP_ACCEPTED = 202;
+    const HTTP_NOTFOUND = 404;
+    const HTTP_BADREQUEST = 400;
+    const HTTP_SERVERERR = 500;
 
     private $sqlite;
     private $skillNumberMapper;
@@ -41,9 +46,9 @@ class WurmController {
 
         $response = $this->sqlite->setSkill($playerId, $skillNumber, $newValue);
         if ($response) {
-            http_response_code(202);
+            http_response_code(static::HTTP_ACCEPTED);
         } else {
-            http_response_code(400);
+            http_response_code(static::HTTP_BADREQUEST);
         }
     }
 
@@ -55,11 +60,14 @@ class WurmController {
         $data = json_decode($jsonBody);
         $player = (string) $data->player;
         $playerId = $this->sqlite->getPlayerIdByName($player);
+        if (!$playerId) {
+            http_response_code(static::HTTP_NOTFOUND);
+        }
         
         $response = $this->sqlite->getSkillsByPlayerId($playerId);
         $augmented = $this->helper->remapAllSkillsWithNames($response, $this->skillNumberMapper);
         
-        http_response_code(200);
+        http_response_code(static::HTTP_OK);
         echo json_encode($augmented);
     }
 
