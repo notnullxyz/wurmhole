6da37d04ab9ac6c96a7ef30c59d55149660a1fc2
M v/d Linde <marlonvdl@curve.group>
2017-09-22 15:25:54 +0200

    backup old index and wire up Silex

--- index.php
+++ index.php
@@ -1,49 +1,12 @@
 <?php
 
-require_once 'WurmController.php';
+// web/index.php
+require_once __DIR__.'/vendor/autoload.php';
 
-// check for action=
-$player = isset($_REQUEST['action']) ? strval($_REQUEST['action']) : null;
-if (!$player) {
-    throw new ErrorException('Query Parameter action= is required');
-}
+$app = new Silex\Application();
 
-/**
- * ..... Now for some quick home-routing without a framework .....
- */
+$app->get('/test/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-
-// only care about params
-if (($pos = strpos($requestUri, "?")) !== FALSE) {
-    $queryParams = substr($requestUri, $pos + 1);
-}
-
-parse_str($queryParams, $params);
-
-$action = $params['action'];
-$body = file_get_contents('php://input');
-$wurmCon = new WurmController();
-
-switch ($requestMethod) {
-    case 'GET':
-        if ($action == 'allSkills') {
-            $wurmCon->getAllSkillsForPlayer($body);
-        } elseif ($action == 'allData') {
-            $wurmCon->getAllDataForPlayer($body);
-        }
-        break;
-
-    case 'PUT':
-        if ($action == 'putSkill') {
-            $wurmCon->updateSingleSkillForPlayer($body);
-        } elseif ($action == 'putPlayerData') {    
-            $wurmCon->updatePlayerDataParameter($body);
-        }      
-        break;
-
-    default:
-        throw new Exception("Improper usage.");
-}
-
+$app->run();
